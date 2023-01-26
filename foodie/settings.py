368b3e743b2673aed92a3884a0726646bf91fef0
Pradeep060401 <109495311+Pradeep060401@users.noreply.github.com>
2023-01-26 19:57:18 +0530

    changeimage name

--- foodie/settings.py
+++ foodie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(3p1!7t)+ph2j)95wvpi6bc8p-a&scbt*lc(5kk!@*52d@46(k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
