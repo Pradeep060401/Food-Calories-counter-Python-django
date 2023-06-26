76d320fdafab2b734d07826140a7ed34608d8765
Pradeep060401 <109495311+Pradeep060401@users.noreply.github.com>
2023-06-26 20:29:34 +0530

    v+2.23.6.26.20:30

--- counter/views.py
+++ counter/views.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
@@ -7,23 +9,31 @@ from django.http import HttpResponse
 # wp80FpjEleF0CHYlW1BEIw==ogoqwPdAM0HQ350E
 # Create your views here.
 
+def getdata(query):
+
+    api_url = f'https://api.api-ninjas.com/v1/nutrition?query={query}'
+    api_request = requests.get(
+        api_url, headers={'X-Api-Key': 'wp80FpjEleF0CHYlW1BEIw==ogoqwPdAM0HQ350E'})
+    try:
+        print(api_request.content, "ss")
+        api = json.loads(api_request.content)
+        return api
+
+
+    except Exception as e:
+        print(e)
+        api = "oops!  "
+        if api=="oops! ":
+            return redirect('/')
+        return api
 
 def home(request):
-    import json
-    import requests
+
     if request.method == 'POST':
         query = request.POST['query']
-        api_url = f'https://api.api-ninjas.com/v1/nutrition?query={query}'
-        api_request = requests.get(
-            api_url, headers={'X-Api-Key': 'wp80FpjEleF0CHYlW1BEIw==ogoqwPdAM0HQ350E'})
-        try:
-            api = json.loads(api_request.content)
-            print(api_request.content, "ss")
 
-        except Exception as e:
-            api = "oops!  "
-            print(e)
-            return redirect('/')
+        api=getdata(query)
+
         if api:
             n=len(api)
             print(n)
